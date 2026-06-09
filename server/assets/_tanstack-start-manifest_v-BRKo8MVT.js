4ba709a279ca1fc1caa9fd00a4109fc78704d842
Somesetty manideep <Manideep723@gmail.com>
2026-06-09 16:15:36 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-BRKo8MVT.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/development projects/metro-auto-bloom/src/routes/__root.tsx", children: ["/", "/privacy-policy"], preloads: ["/assets/index-CnMLk4Gp.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-CnMLk4Gp.js" } }] }, "/": { filePath: "C:/development projects/metro-auto-bloom/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-4b2zWsc2.js", "/assets/MetroSections-xNCi82Hd.js"] }, "/privacy-policy": { filePath: "C:/development projects/metro-auto-bloom/src/routes/privacy-policy.tsx", children: void 0, preloads: ["/assets/privacy-policy-BrRt2vaJ.js", "/assets/MetroSections-xNCi82Hd.js"] } } });
+export {
+  tsrStartManifest
+};
